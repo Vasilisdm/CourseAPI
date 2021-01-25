61323d36f1cc768d2d31163b4746232d66686dd0
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-25 21:54:49 +0200

    Created the authorsController class alongside with the GetAuthors method.

---
+++ API/Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AuthorsController : ControllerBase
+    {
+        private ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(CourseLibraryRepository));
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authors = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authors);
+        }
+    }
+}
