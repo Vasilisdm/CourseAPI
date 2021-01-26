6bb17416d927bf7b0d08234f5f1cd9f0c6a7cf9a
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-26 22:21:12 +0200

    Created the authorDTO which will be used in the outer facing contract.

---
+++ API/Models/AuthorDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace API.Models
+{
+    public class AuthorDTO
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string MainCategory { get; set; }
+    }
+}
