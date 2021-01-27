2a9cf5db7e8a6fb8d610271d38a5d698fb0e482c
vasilisdimitriou <vasilisdmr@gmail.com>
2021-01-27 21:56:59 +0200

    Created the backbone for the coursesController.

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository;
+            _mapper = mapper;
+        }
+    }
+}
