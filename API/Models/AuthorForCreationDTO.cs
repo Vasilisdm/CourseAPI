c6f56c2a2b4fe52323b6d67148a8f4642658549d
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-10 22:58:24 +0200

    Created AuthorForCreationDTO which is going to be used with the new CreateAuthor method as the input parameter's type.

---
+++ API/Models/AuthorForCreationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace API.Models
+{
+    public class AuthorForCreationDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string MainCategory { get; set; }
+    }
+}
