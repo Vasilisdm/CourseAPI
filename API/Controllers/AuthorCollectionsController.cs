6bff0b6b99b800c1a79c6f4c6319d8fee4598fc1
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-13 21:27:43 +0200

    Created AuthorCollectionsController in order to create to be able to create a collection of authors at once.

---
+++ API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using API.Models;
+using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private ICourseLibraryRepository _courseLibraryRepository;
+        private IMapper _mapper;
+
+        public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(
+                nameof(CourseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(Mapper));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDTO>> CreateAuthorCollection(
+            IEnumerable<AuthorForCreationDTO> authorCollection)
+        {
+            var authorEntities = _mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+
+            _courseLibraryRepository.Save();
+
+            return Ok();
+        }
+    }
+}
