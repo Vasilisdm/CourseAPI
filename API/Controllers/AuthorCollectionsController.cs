80667e0a2990689c58157d1eae9564c111e22e5b
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-13 22:03:12 +0200

    Implemented GetAuthorCollection which returns the AuthorDTO models for the given IEnumerable of guids.

--- API/Controllers/AuthorCollectionsController.cs
+++ API/Controllers/AuthorCollectionsController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using API.Helpers;
 using API.Models;
 using AutoMapper;
 using CourseLibrary.API.Entities;
@@ -21,6 +23,27 @@ namespace API.Controllers
             _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(
                 nameof(CourseLibraryRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(Mapper));
+        }
+
+        [HttpGet("({ids})")]
+        public IActionResult GetAuthorCollection(
+        [FromRoute]
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> guids)
+        {
+            if (guids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _courseLibraryRepository.GetAuthors(guids);
+            if (guids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDTO>>(authorEntities);
+
+            return Ok(authorsToReturn);
         }
 
         [HttpPost]
