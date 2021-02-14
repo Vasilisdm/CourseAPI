ce5d29b2cf2f38a860d79b0adc5725597c0e6347
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-14 18:21:38 +0200

    Added Annotations on CourseForCreationDTO in order to set the title as required and give maxLength both to title and to description

--- API/Models/CourseForCreationDTO.cs
+++ API/Models/CourseForCreationDTO.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Models
 {
     public class CourseForCreationDTO
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
